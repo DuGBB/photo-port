886b51299836a7e3d1d704129dacde1c884d1da6
Donald Biggers <DuG@tests-MacBook-Air.local>
2022-02-25 10:33:30 -0600

    completed photo gallery functionality

---
+++ src/components/PhotoList/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import PhotoList from "../";
+
+// const portrait = {
+//   name: "portraits",
+//   description: "Portraits of people in my life",
+// };
+
+afterEach(cleanup);
+
+describe("PhotoList is rendering", () => {
+  it("renders", () => {
+    render(<PhotoList />);
+  });
+
+  //   it("matches snapshot", () => {
+  //     const { asFragment } = render(<PhotoList currentCategory={portrait} />);
+  //     expect(asFragment()).toMatchSnapshot();
+  //   });
+
+  it("renders", () => {
+    const { asFragment } = render(<PhotoList />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
